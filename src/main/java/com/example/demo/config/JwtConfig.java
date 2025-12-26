f21380258a8a483fe3a178ebd5e6beba5e308bcb
bala-amypo <bala@amypo.in>
2025-12-26 08:46:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/JwtConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import com.example.demo.security.JwtUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil(
+                "abcdefghijklmnopqrstuvwxyz0123456789ABCD", // SAME AS TEST
+                3600000L // 1 hour (SAME AS TEST)
+        );
+    }
+}
